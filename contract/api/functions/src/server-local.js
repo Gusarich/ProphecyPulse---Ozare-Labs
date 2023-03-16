106bd5ce8a3cbc0f87dcfd4c49726e112269fc72
Jaiwanth <jaiwanth2011@gmail.com>
2023-03-17 00:00:13 +0530

    TonConnect react -> server

--- contract/api/functions/src/server-local.js
+++ contract/api/functions/src/server-local.js
@@ -8,11 +8,18 @@ const index_3 = require("ton-core")
 const Event_1 = require("./wrappers/Event");
 const Bet = require('./wrappers/Bet');
 const Livescore_1 = require("./wrappers/Livescore");
+const cors = require("cors");
+const corsOptions = {
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 require('dotenv').config()
 
 // start the server
-const port = 3000;
+const port = 8000;
 const app = express();
+app.use(cors(corsOptions))
 app.use(bodyParser.json());
 
 app.listen(port, () => {
--- react/ozare/src/Home.tsx
+++ react/ozare/src/Home.tsx
@@ -1,13 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import {
   TonConnectButton,
   useTonWallet,
   useTonAddress,
+  useTonConnectUI,
 } from "@tonconnect/ui-react";
 
 function Home() {
   const userFriendlyAddress = useTonAddress();
   const wallet = useTonWallet();
-
+  const [tonConnectUI] = useTonConnectUI();
+  const [transaction, setTransaction] = useState<any>(null);
   function redirect() {
     if (userFriendlyAddress?.length > 0 && wallet?.name) {
       window.parent.postMessage(
@@ -16,6 +20,27 @@ function Home() {
       );
     }
   }
+
+  async function handleClick() {
+    const serverURL = "http://localhost:8000";
+    const response = await axios.post(`${serverURL}/event`, {
+      via: tonConnectUI.connector,
+      address: userFriendlyAddress,
+    });
+    console.log(response.data);
+  }
+
+  useEffect(() => {
+    // get the transaction details from the parent window
+    const first: any = window.addEventListener("message", (event) => {
+      if (event.data) {
+        setTransaction(event.data);
+      }
+    });
+    return () => {
+      window.removeEventListener("message", first);
+    };
+  }, []);
 
   return (
     <div className="flex bg-white justify-center items-center h-screen w-screen">
@@ -37,7 +62,7 @@ function Home() {
           <div className="flex justify-center">
             <button
               disabled={!(userFriendlyAddress?.length > 0)}
-              onClick={() => redirect()}
+              onClick={handleClick}
               className="px-8 rounded-2xl py-4 bg-blue-500 text-white disabled:bg-gray-300"
             >
               Continue
