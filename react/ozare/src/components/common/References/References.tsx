fccc14edd47b94026c4741f784fca18ffc0a0a4c
Jaiwanth <jaiwanth2011@gmail.com>
2023-03-23 08:43:25 +0530

    Revert "feat: new ui"

--- react/ozare/src/components/common/References/References.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const References: React.FC = () => {
-  return <></>;
-};
--- react/ozare/src/ton/Ton.tsx
+++ react/ozare/src/ton/Ton.tsx
@@ -1,11 +1,11 @@
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 // import { Routes, Route } from "react-router-dom";
-// import Home from './Home';
+import Home from "./Home";
 
 function Ton() {
   return (
     <TonConnectUIProvider manifestUrl="https://ozare-final.vercel.app/ton/tonconnect-manifest.json">
-      {/* <Home /> */}
+      <Home />
     </TonConnectUIProvider>
   );
 }
