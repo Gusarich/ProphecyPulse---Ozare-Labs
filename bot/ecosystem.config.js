202e8103bbbb9e4e6251fac3707f63329b023403
Jaiwanth <jaiwanth2011@gmail.com>
2023-03-25 10:48:32 +0530

    feat: add pm2 config for bot deployment

---
+++ bot/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "telegram-bot",
+      script: "npm",
+      args: "start",
+      cwd: "/root/ProphecyPulse---Ozare-Labs/bot", // replace it if you deploy in another directory
+    },
+  ],
+};
