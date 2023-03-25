409453f7952dc035d6f80961f36a1978dc4cef5b
Innocent Masuki <innocentmasuki99@gmail.com>
2023-03-25 09:15:43 +0300

    chore: Added Teams images

--- react/ozare/src/Pages/Home/Home.tsx
+++ react/ozare/src/Pages/Home/Home.tsx
@@ -82,44 +82,46 @@ function Home() {
         </div>
       </Header>
 
-      <section>
-        <Tab.Group>
-          <Tab.List
-            className={
-              "flex sticky top-[148px]  bg-white flex-row py-4 justify-center"
-            }
-          >
-            {menuItems.map((item) => (
-              <Tab
-                key={item.title}
-                onClick={() => setContentCategory(item.title)}
-                className={({ selected }) =>
-                  selected
-                    ? "bg-sky-400 outline-none text-white shadow-sky-100 shadow-lg flex flex-row justify-start px-2 py-1 items-center rounded-full mx-2"
-                    : "text-sky-400 bg-white shadow-sky-100 shadow-lg flex flex-row justify-start px-2 py-1 items-center rounded-full mx-2"
-                }
-              >
-                <div className={"pr-2"}>{item.icon}</div>
-                <div className={"text-sm font-bold pr-2"}>{item.title}</div>
-              </Tab>
-            ))}
-          </Tab.List>
-          <Tab.Panels>
-            {menuItems.map((item) => {
-              return (
-                <Tab.Panel key={item.title}>
-                  {contentCategory === item.title && (
-                    <Matches
-                      data={matches}
-                      category={contentCategory.toLowerCase()}
-                    />
-                  )}
-                </Tab.Panel>
-              );
-            })}
-          </Tab.Panels>
-        </Tab.Group>
-      </section>
+      {!(searchQuery.length > 0) && (
+        <section>
+          <Tab.Group>
+            <Tab.List
+              className={
+                "flex sticky top-[148px]  bg-white flex-row py-4 justify-center"
+              }
+            >
+              {menuItems.map((item) => (
+                <Tab
+                  key={item.title}
+                  onClick={() => setContentCategory(item.title)}
+                  className={({ selected }) =>
+                    selected
+                      ? "bg-sky-400 outline-none text-white shadow-sky-100 shadow-lg flex flex-row justify-start px-2 py-1 items-center rounded-full mx-2"
+                      : "text-sky-400 bg-white shadow-sky-100 shadow-lg flex flex-row justify-start px-2 py-1 items-center rounded-full mx-2"
+                  }
+                >
+                  <div className={"pr-2"}>{item.icon}</div>
+                  <div className={"text-sm font-bold pr-2"}>{item.title}</div>
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels>
+              {menuItems.map((item) => {
+                return (
+                  <Tab.Panel key={item.title}>
+                    {contentCategory === item.title && (
+                      <Matches
+                        data={matches}
+                        category={contentCategory.toLowerCase()}
+                      />
+                    )}
+                  </Tab.Panel>
+                );
+              })}
+            </Tab.Panels>
+          </Tab.Group>
+        </section>
+      )}
     </>
   );
 }
