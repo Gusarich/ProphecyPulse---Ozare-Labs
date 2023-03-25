16838d1656ea2511bb72ca768ad8827c8f67c771
Innocent Masuki <innocentmasuki99@gmail.com>
2023-03-25 07:01:03 +0300

    chore: Fixed link error

--- react/ozare/src/Pages/Home/Matches.tsx
+++ react/ozare/src/Pages/Home/Matches.tsx
@@ -34,48 +34,46 @@ function Matches({ data, category }: MatchesProps) {
 
           <div className="flex w-full flex-row justify-start overflow-x-auto scrollbar-hide">
             {stage.Events.filter((event) => event.Eps !== "FT").map((event) => (
-              <Link
-                to={`/match/${category}/${event.Eid}`}
-                key={event.Eid}
-                className="overflow-hidden relative -z-10 bg-black min-w-[250px] mx-4 rounded-2xl shadow-sm mt-4 h-40 "
-              >
-                <div className="h-full w-full opacity-60">
-                  <img
-                    src={
-                      category === "soccer"
-                        ? SoccerBackground
-                        : category === "cricket"
-                        ? CricketBackground
-                        : BasketballBackground
-                    }
-                    className="object-cover bg-no-repeat h-full w-full "
-                    alt=""
-                  />
-                </div>
+              <Link to={`/match/${category}/${event.Eid}`} key={event.Eid}>
+                <div className="overflow-hidden relative -z-10 bg-black min-w-[250px] mx-4 rounded-2xl shadow-sm mt-4 h-40 ">
+                  <div className="h-full w-full opacity-60">
+                    <img
+                      src={
+                        category === "soccer"
+                          ? SoccerBackground
+                          : category === "cricket"
+                          ? CricketBackground
+                          : BasketballBackground
+                      }
+                      className="object-cover bg-no-repeat h-full w-full "
+                      alt=""
+                    />
+                  </div>
 
-                <div className="absolute h-full w-full top-0 left-0">
-                  <div className="relative h-full w-full">
-                    <div className="h-full grid grid-cols-3 px-2">
-                      <div className="flex flex-col items-center text-xs text-center text-white font-bold justify-center">
-                        {event.T1[0].Nm}
+                  <div className="absolute h-full w-full top-0 left-0">
+                    <div className="relative h-full w-full">
+                      <div className="h-full grid grid-cols-3 px-2">
+                        <div className="flex flex-col items-center text-xs text-center text-white font-bold justify-center">
+                          {event.T1[0].Nm}
+                        </div>
+                        <div className="text-white text-2xl flex flex-col text-center justify-center items-center font-bold mx-4">
+                          VS
+                        </div>
+                        <div className="flex flex-col justify-center text-xs text-white font-bold items-center text-center">
+                          {event.T2[0].Nm}
+                        </div>
                       </div>
-                      <div className="text-white text-2xl flex flex-col text-center justify-center items-center font-bold mx-4">
-                        VS
+                      <div className="absolute top-0 w-full text-center text-xs text-white">
+                        <span className="bg-sky-500 px-2 py-1 rounded-b-xl">
+                          {event.Eps === "NS"
+                            ? "Upcoming"
+                            : event.Eps === "HT"
+                            ? "Playing Now"
+                            : event.Eps === "FT"
+                            ? "Ended"
+                            : "Upcoming"}
+                        </span>
                       </div>
-                      <div className="flex flex-col justify-center text-xs text-white font-bold items-center text-center">
-                        {event.T2[0].Nm}
-                      </div>
-                    </div>
-                    <div className="absolute top-0 w-full text-center text-xs text-white">
-                      <span className="bg-sky-500 px-2 py-1 rounded-b-xl">
-                        {event.Eps === "NS"
-                          ? "Upcoming"
-                          : event.Eps === "HT"
-                          ? "Playing Now"
-                          : event.Eps === "FT"
-                          ? "Ended"
-                          : "Upcoming"}
-                      </span>
                     </div>
                   </div>
                 </div>
